d1198e6e82152a02e077f102c70aed33f5f049ec
James Milner <james.milner@pearson.com>
2019-01-14 11:02:29 +0000

    first commit

---
+++ tetris.rb
@@ -0,0 +1,217 @@
+#!/usr/bin/env ruby
+
+require "curses"
+
+Curses.init_screen
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 30
+
+FIELD_WIDTH = 12
+FIELD_HEIGHT = 18
+
+FIELD = []
+(0...FIELD_WIDTH).each do |x|
+  (0...FIELD_HEIGHT).each do |y|
+    FIELD[y * FIELD_WIDTH + x] = (x == 0 || x == FIELD_WIDTH - 1 || y == FIELD_HEIGHT - 1) ? 9 : 0
+  end
+end
+
+TETRONIMO = Array.new(7) { Array.new }
+
+TETRONIMO[0] += "..X.".chars
+TETRONIMO[0] += "..X.".chars
+TETRONIMO[0] += "..X.".chars
+TETRONIMO[0] += "..X.".chars
+
+TETRONIMO[1] += "..X.".chars
+TETRONIMO[1] += ".XX.".chars
+TETRONIMO[1] += ".X..".chars
+TETRONIMO[1] += "....".chars
+
+TETRONIMO[2] += ".X..".chars
+TETRONIMO[2] += ".XX.".chars
+TETRONIMO[2] += "..X.".chars
+TETRONIMO[2] += "..X.".chars
+
+TETRONIMO[3] += "....".chars
+TETRONIMO[3] += ".XX.".chars
+TETRONIMO[3] += ".XX.".chars
+TETRONIMO[3] += "....".chars
+
+TETRONIMO[4] += "..X.".chars
+TETRONIMO[4] += ".XX.".chars
+TETRONIMO[4] += "..X.".chars
+TETRONIMO[4] += "....".chars
+
+TETRONIMO[5] += "....".chars
+TETRONIMO[5] += ".XX.".chars
+TETRONIMO[5] += "..X.".chars
+TETRONIMO[5] += "..X.".chars
+
+TETRONIMO[6] += ".XX.".chars
+TETRONIMO[6] += ".X..".chars
+TETRONIMO[6] += ".X..".chars
+TETRONIMO[6] += ".X..".chars
+
+
+
+def rotate(x,y,r)
+  case (r % 4)
+  when 0
+    return y * 4 + x
+  when 1
+    return 12 + y - (x * 4)
+  when 2
+    return 15 - (y * 4) - x
+  when 3
+    return 3 - y + (x * 4)
+  else
+    return 0
+  end
+end
+
+def does_piece_fit(id, rotation, posx, posy)
+  (0...4).each do |x|
+    (0...4).each do |y|
+      pi = rotate(x,y,rotation)
+      fi = (posy + y) * FIELD_WIDTH + (posx + x)
+      if posx + x >= 0 && posx + x < FIELD_WIDTH
+        if posy + y >= 0 && posy + y < FIELD_HEIGHT
+          return false if (TETRONIMO[id][pi] == 'X' && FIELD[fi] != 0)
+        end
+      end
+    end
+  end
+  return true
+end
+
+begin
+
+  win = Curses::Window.new(SCREEN_HEIGHT, SCREEN_WIDTH, 0, 0)
+  win.keypad = true
+  win.timeout = 5
+  win.nodelay = true
+
+  game_over = false
+
+  current_piece = rand(6)
+  current_rotation = 0
+  current_x = FIELD_WIDTH / 2
+  current_y = 0
+  speed = 20
+  speed_count = 0
+  piece_count = 0
+  score = 0
+  lines = []
+  force_down = false
+
+  while (!game_over) do
+
+    # Timing
+    sleep(0.05)
+    speed_count += 1
+    force_down = (speed_count == speed)
+
+    # Input
+    key = win.getch
+
+    # Logic
+    current_x -= key == Curses::Key::LEFT && does_piece_fit(current_piece, current_rotation, current_x - 1, current_y) ? 1 : 0
+    current_x += key == Curses::Key::RIGHT && does_piece_fit(current_piece, current_rotation, current_x + 1, current_y) ? 1 : 0
+    current_y += key == Curses::Key::DOWN && does_piece_fit(current_piece, current_rotation, current_x, current_y + 1) ? 1 : 0
+    current_rotation += key == Curses::Key::UP && does_piece_fit(current_piece, current_rotation + 1, current_x, current_y) ? 1 : 0
+
+    if force_down
+      speed_count = 0
+			piece_count += 1
+			if (piece_count % 50 == 0) && speed >= 10
+        speed -= 1
+      end
+      if does_piece_fit(current_piece, current_rotation, current_x, current_y + 1)
+        current_y += 1
+      else
+        # Lock Piece
+        (0...4).each do |x|
+          (0...4).each do |y|
+            if TETRONIMO[current_piece][rotate(x,y,current_rotation)] != '.'
+              FIELD[(current_y + y) * FIELD_WIDTH + (current_x + x)] = current_piece + 1
+            end
+          end
+        end
+        # Check for lines
+        (0...4).each do |y|
+          if (current_y + y < FIELD_HEIGHT - 1)
+            line = true
+            (1...FIELD_WIDTH - 1).each do |x|
+              line = line && ((FIELD[(current_y + y) * FIELD_WIDTH + x]) != 0)
+            end
+            if line
+              (1...FIELD_WIDTH - 1).each do |x|
+                FIELD[(current_y + y) * FIELD_WIDTH + x] = 8
+              end
+              lines << (current_y + y)
+            end
+          end
+        end
+        score += 25
+        score += (1 << lines.size) * 100 unless lines.empty?
+        current_x = FIELD_WIDTH / 2
+        current_y = 0
+        current_rotation = 0
+        current_piece = rand(6)
+        game_over = !does_piece_fit(current_piece, current_rotation, current_x, current_y)
+      end
+    end
+
+    # Render
+    screen = Array.new(SCREEN_WIDTH * SCREEN_HEIGHT) { " " }
+
+    # Draw FIELD
+    (0...FIELD_WIDTH).each do |x|
+      (0...FIELD_HEIGHT).each do |y|
+        screen[(y + 2) * SCREEN_WIDTH + (x + 2)] = " ABCDEFG=#".chars[FIELD[y * FIELD_WIDTH + x]]
+      end
+    end
+
+    # Draw Piece
+    (0...4).each do |x|
+      (0...4).each do |y|
+        if TETRONIMO[current_piece][rotate(x,y,current_rotation)] == 'X'
+          screen[(current_y + y + 2) * SCREEN_WIDTH + (current_x + x + 2)] = (current_piece + 65).chr
+        end
+      end
+    end
+
+    score_string = ("SCORE: %8d" % score).chars
+    score_start = 2 * SCREEN_WIDTH + FIELD_WIDTH + 6
+    score_end = score_start + score_string.size
+    screen[score_start...score_end] = score_string
+
+    # Remove Lines
+    if !lines.empty?
+      sleep(0.4)
+      lines.each do |l|
+        (1...FIELD_WIDTH - 1).each do |x|
+          (l).downto(0) do |y|
+            FIELD[y * FIELD_WIDTH + x] = FIELD[(y - 1) * FIELD_WIDTH + x]
+          end
+          FIELD[x] = 0
+        end
+      end
+			lines.clear
+    end
+
+    # Update Screen
+    win.clear
+    win.addstr(screen.join)
+    win.refresh
+
+  end
+
+  puts "Game Over!! Score: %8d" % score
+  sleep(10)
+
+ensure
+  Curses.close_screen
+end
